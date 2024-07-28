ca0e6641d11eca277f94f901baaa036c68386319
bleaz77 <flmarwan999@gmail.com>
2024-07-28 01:25:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeMessage() {
+    const messageElement = document.getElementById('message');
+    const imageElement = document.getElementById('surpriseImage');
+
+    messageElement.textContent = "I love you more each day!";
+    imageElement.style.display = 'block';
+
+}
